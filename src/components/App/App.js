258fad169785caf100fd9f4ecc23ec6635952d92
Colin Ciervo <colinciervo@@gmail.com>
2023-01-02 15:43:40 -0500

    initial set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,23 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tricks: ''
+    }
+  }
+
+  componentDidMount() {
+    
+  }
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <div className="tricks">
+          <p>{this.state.tricks}</p>
+        </div>
       </div>
     );
   }
